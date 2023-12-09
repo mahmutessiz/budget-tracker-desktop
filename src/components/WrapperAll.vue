4d4626bbe0eb3128b3673860fe7704f890cda361
mahmutessiz <mahmutessiz71@gmail.com>
2023-12-09 12:48:16 +0300

    add pinia for state management

--- src/components/WrapperAll.vue
+++ src/components/WrapperAll.vue
@@ -1,19 +1,21 @@
 <template>
   <div class="w-full flex flex-wrap lg:flex-nowrap overflow-x-hidden">
     <MainHeader class="fixed z-10 w-32" />
-    <div class="w-full md:w-[80%] lg:w-[70%] overflow-x-hidden ml-36">
-      <BalanceDisplay />
-      <BarChart />
-      <div class="flex flex-wrap md:flex-nowrap gap-4 md:gap-1 mt-12">
-        <DoughnutChart class="md:w-[50%]" />
-        <LineChart class="md:w-[50%]" />
+    <div class="w-full flex flex-wrap lg:flex-nowrap justify-end">
+      <div class="w-full md:w-[80%] lg:w-[70%] overflow-x-hidden ml-40">
+        <BalanceDisplay />
+        <BarChart />
+        <div class="flex flex-wrap md:flex-nowrap gap-4 md:gap-1 mt-12">
+          <DoughnutChart class="md:w-[50%]" />
+          <LineChart class="md:w-[50%]" />
+        </div>
       </div>
-    </div>
-    <div
-      class="w-full lg:w-[28%] flex lg:flex-col justify-center items-start gap-12 mt-12 mb-12 ml-36 lg:ml-8"
-    >
-      <TableIncome />
-      <TableExpense />
+      <div
+        class="w-full lg:w-[15%] flex lg:flex-col justify-center items-start gap-12 mt-12 mb-12 ml-36 lg:ml-8"
+      >
+        <TableIncome />
+        <TableExpense />
+      </div>
     </div>
   </div>
 </template>
--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,9 @@
 import { createApp } from "vue";
+import { createPinia } from "pinia";
 import router from "./router";
 import "./styles.css";
 
+const pinia = createPinia();
 import App from "./App.vue";
 
-createApp(App).use(router).mount("#app");
+createApp(App).use(router).use(pinia).mount("#app");
